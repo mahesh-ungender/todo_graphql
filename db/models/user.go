802c6a26372f17f9943402fc833d31d59f669994
Akshaj Dave <akshaj.dave@gmail.com>
2023-10-06 14:48:50 +0530

    Conv to string

--- db/models/user.go
+++ db/models/user.go
@@ -1,15 +1,16 @@
 package models
 
 import (
-	"github.com/astaxie/beego/orm"
 	graphmodel "todo_graphql/graph/model"
+
+	"github.com/astaxie/beego/orm"
 )
 
 // todo model for todo
 type Todo struct {
-	ID                       int64             `orm:"pk;auto;column(id)" json:"id"`
-	ItemName                 *string           `json:"itemname" orm:"null"`
-	Status                   *string           `json:"status" orm:"null"`
+	ID       int64   `orm:"pk;auto;column(id)" json:"id"`
+	ItemName *string `json:"itemname" orm:"null"`
+	Status   *string `json:"status" orm:"null"`
 }
 
 // TableName specifies the table name for User in the db
@@ -20,15 +21,14 @@ func (t *Todo) TableName() string {
 // Serialize serializes the user model to response
 func (t *Todo) Serialize() *graphmodel.Todo {
 	res := &graphmodel.Todo{
-		ID:              t.ID,
-		ItemName:        *t.ItemName,
-		Status:          *t.Status,
+		ID:       string(t.ID),
+		ItemName: *t.ItemName,
+		Status:   *t.Status,
 	}
 	return res
 }
 
-
-//register the tablw
+// register the tablw
 func init() {
 	orm.RegisterModel(new(Todo))
 }
